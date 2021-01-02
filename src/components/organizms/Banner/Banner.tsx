a992b6c7847fd3dda7e79b3d9f0ff381dd1829bd
Soshnikov Aleskandr <innerhard@gmail.com>
2021-01-02 04:50:10 +0300

    refactor banner component

--- src/components/organizms/Banner/Banner.tsx
+++ src/components/organizms/Banner/Banner.tsx
@@ -10,8 +10,12 @@ interface DataProps {
 export const Banner: FC<DataProps> = ({ img, title, description }) => {
     return (
         <Styled.WrapperCard img={img}>
-            {title}
-            {description}
+            <Styled.WrapperText variant="h1" gutterBottom color="primary">
+                {title}
+            </Styled.WrapperText>
+            <Styled.WrapperText variant="h5" gutterBottom color="primary">
+                {description}
+            </Styled.WrapperText>
         </Styled.WrapperCard>
     )
 }
